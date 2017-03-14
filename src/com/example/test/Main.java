310779b087c4f75561fc57259b9f2e92c7accb43
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-03-14 21:23:58 +0300

    Первая программа на java

--- src/com/example/test/Main.java
+++ src/com/example/test/Main.java
@@ -1,9 +1,7 @@
 package com.example.test;
 
 public class Main {
-
     public static void main(String[] args) {
-
 
     }
 }
---
+++ src/example/chapter1/HelloWorld.java
@@ -0,0 +1,10 @@
+package example.chapter1;
+
+/**
+ * Created by Гамзат on 3/14/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
